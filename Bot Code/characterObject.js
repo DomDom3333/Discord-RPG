e1596a5ac6d63768f7c5665a94583ca814c98988
DomDom3333 <Dominik.essenhofer@gmail.com>
2020-06-24 15:54:45 +0200

    Character file upload now possible

---
+++ Bot Code/characterObject.js
@@ -0,0 +1,16 @@
+module.exports = {
+    name: '',
+    gender: '',
+    race:'',
+    stats:'',
+    load(message,args){
+        loadChar(message,args);
+    },
+}
+
+function save(message,args){
+    JSON.stringify()
+}
+function loadChar(message,args){
+    var dir = './Resources/Servers/' + message.guild.id + '/' + message.author.id + '/CharFiles/' // + character/filename
+}
--- Bot Code/commands/create.js
+++ Bot Code/commands/create.js
@@ -1,19 +1,20 @@
 let request = require('request');
 let fs = require(`fs`);
 let shelljs = require('shelljs');
+let character = require('characterObject.js');
 
 module.exports = {
     name: 'create',
     description: "creates a Character,game or Party",
     execute(message,args){
         if(args[1] === "character"){
-
+            createCharacter(message,args);
         }
         else if (args[1] === "game"){
             createGame(message,args);
         }
         else if (args[1] === "party"){
-
+            message.channel.send("Not yet implimented");
         }
         else{
             message.channel.send("Please specify waht you want to create. (Character/Game/Party)")
@@ -29,7 +30,7 @@ function createGame(message, args){
         if(filename.substring(filename.length-4) === 'json'){
             shelljs.mkdir('-p', './Resources/Servers/' + message.guild.id + '/' + message.author.id + '/GameFiles/' + args[2]);
             console.log("filname = json");
-            download(message,args,dir);
+            downloadGame(message,args,dir);
         }
         else{
             message.channel.send("Please only upload JSON files. All other will be ignored")
@@ -37,11 +38,32 @@ function createGame(message, args){
     }
 }
 
-function download(message,args,dir){
-    console.log("trying to fetch stuff")
+function downloadGame(message,args,dir){
     var url = message.attachments.array()[0].url;
     request.get(url)
         .on('error', console.error)
         .pipe(fs.createWriteStream(dir + '/' + args[2] + '.json'));
-    console.log("fetched stuff");
+}
+
+function createCharacter(message,args){
+    var dir = './Resources/Servers/' + message.guild.id + '/' + message.author.id + '/CharFiles/' + args[2];
+    var filename = message.attachments.array()[0].name;
+    filename = filename.toLowerCase();
+    if (args[2] != null){
+        if(filename.substring(filename.length-4) === 'json'){
+            shelljs.mkdir('-p', './Resources/Servers/' + message.guild.id + '/' + message.author.id + '/CharFiles/' + args[2]);
+            console.log("filname = json");
+            downloadChar(message,args,dir);
+        }
+        else{
+            message.channel.send("Please only upload JSON files. All other will be ignored")
+        }
+    }
+}
+
+function downloadChar(message,args,dir){
+    var url = message.attachments.array()[0].url;
+    request.get(url)
+        .on('error', console.error)
+        .pipe(fs.createWriteStream(dir + '/' + args[2] + '.json'));
 }
